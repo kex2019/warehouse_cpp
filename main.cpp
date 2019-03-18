58f62f56bba598bb70ad4eb2f7db967333a191fc
Johan Vikström <johan.vikstrom7@gmail.com>
2019-03-18 17:35:56 +0100

    Added result handler

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <string>
 #include <fstream>
+#include <ctime>
 #include "warehouse.h"
 #include "algos/cw.h"
 #include "algos/greedy.h"
@@ -18,19 +19,41 @@ vector<long> generateSeeds(int N) {
     return seeds;
 }
 
-void exportStatistics(const vector<int>& results, int nRobots, int robotCapacity, const vector<long> &seeds) {
-
-}
-
+class ResultHandler {
+    string name;
+    ofstream resultFile;
+public:
+    ResultHandler(string dir, std::string name, bool overwrite = false) : name(name) {
+        auto flags = ios::out;
+        if(overwrite)
+            flags = flags | ios::trunc;
+        resultFile.open(dir + "/" + name, flags);
+        if(!resultFile.is_open()) {
+            cerr << "Could not open " << (dir + "/" + name) << " for writing results, throwing" << endl;
+            throw runtime_error("Could not open result file"); 
+        }
+    }
+    ~ResultHandler() {
+        resultFile.close();
+    }
+    void appendResult(int result, int nRobots, int robotCapacity, int seed, long millis) {
+        resultFile << result << "," << nRobots << "," << robotCapacity << "," << seed << "," << millis << "\n"; 
+        resultFile.flush();
+    }
+};
 
 template<typename T>
-vector<int> run(T t, string machineName, const WarehouseInfo& info, int nRobots, int robotCapacity, const vector<long>& seeds) {
+vector<int> run(ResultHandler &resultHandler, T t, const WarehouseInfo& info, int nRobots, int robotCapacity, const vector<long>& seeds) {
     vector<int> results(seeds.size());
     for(int i = 0; i < seeds.size(); i++) {
+        clock_t begin = clock();
         Warehouse warehouse = generateRandomWarehouse(info, seeds[i]);
         auto batches = t.solve(robotCapacity, warehouse);
         int solTime = evaluateSolutionTime(warehouse, batches, nRobots, robotCapacity);
         results[i] = solTime;
+        clock_t end = clock();
+        double elapsedMs = double(end - begin) * 1000.0 / CLOCKS_PER_SEC ;
+        resultHandler.appendResult(solTime, nRobots, robotCapacity, (int)seeds[i], (long)elapsedMs);
     }
     return results;
 }
@@ -46,10 +69,14 @@ int main() {
 
     auto G = ga::Ga(10);
 
+    ResultHandler cwsr("results", "cws");
+    ResultHandler greedyr("results", "greedy");
+    ResultHandler gar("results", "ga");
+
     auto seeds = generateSeeds(100);
-    auto cws = run(cw::cw(), "cws", info, 2, 5, seeds);
-    auto greedys = run(greedy::greedy(), "greedy", info, 2, 5, seeds);
-    auto ga = run(G, "ga", info, 2, 5, seeds);
+    auto cws = run(cwsr, cw::cw(), info, 2, 5, seeds);
+    auto greedys = run(greedyr, greedy::greedy(), info, 2, 5, seeds);
+    auto ga = run(gar, G, info, 2, 5, seeds);
 
     int accCWS = 0;
     int accGreedys = 0;
