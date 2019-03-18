47c5a4192f7f76d65c4b7c519f09c46a3cd79668
jonas@valfridsson.net <jonas@valfridsson.net>
2019-03-18 13:22:10 +0100

    GA header

--- algos/ga.h
+++ algos/ga.h
@@ -1,5 +1,25 @@
 #pragma once
 
+#include <vector>
+#include "../warehouse.h"
+
+
+using namespace std;
+namespace ga {
+
+  typedef vector<vector<int>> Chromosomes;
+
+  class Ga {
+    int population;
+    // This vector of vector of ints is a splitted chromosome
+    vector<vector<int>> solve(int robotCapacity, const Warehouse &warehouse);
+
+    void fitness(Chromosomes &chromosomes, vector<int> &fitnesses);
+    vector<int> select(Chromosomes &chromosomes, vector<int> &fitnesses);
+    void crossover(Chromosomes &chromosomes, vector<int> elitists);
+    void mutate(Chromosomes &chromosomes, vector<int> elitists);
+  };
+}
 
 
 
