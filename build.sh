682dbae605dd71490ae52910e7e74131261fe101
Jonas Valfridsson <jonas@valfridsson.net>
2019-04-09 15:07:23 +0200

    Prep Run

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #g++ main.cpp warehouse.cpp -std=c++17 -O3 -o warehouse.out
 # -fstack-protector-all
-g++ main.cpp warehouse.cpp algos/cw.cpp algos/greedy.cpp algos/ga.cpp algos/tabu.cpp algos/complsearch.cpp -std=c++17 -O3 -Wall -o warehouse.out
+g++-7 main.cpp warehouse.cpp algos/cw.cpp algos/greedy.cpp algos/ga.cpp algos/tabu.cpp algos/complsearch.cpp -std=c++17 -O3 -Wall -o warehouse.out
